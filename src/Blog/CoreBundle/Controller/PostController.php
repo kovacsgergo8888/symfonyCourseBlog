e00f8cfe518453ecddd0df812cc903967e537668
Kovács Gergely <gergo.kovacs@innonic.com>
2017-03-12 14:36:44 +0100

    comment form

--- src/Blog/CoreBundle/Controller/PostController.php
+++ src/Blog/CoreBundle/Controller/PostController.php
@@ -2,9 +2,13 @@
 
 namespace Blog\CoreBundle\Controller;
 
+use Blog\ModelBundle\Entity\Comment;
 use Blog\ModelBundle\Form\CommentType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -59,10 +63,38 @@ class PostController extends Controller
     /**
      * @param Request $request
      * @param $slug
+     *
+     * @return array|RedirectResponse
+     *
+     * @Route("/{slug}/create-comment")
+     * @Method("POST")
+     * @Template("CoreBundle:Post:show.html.twig")
      */
     public function createCommentAction(Request $request, $slug)
     {
+        $post = $this->getDoctrine()->getRepository('ModelBundle:Post')->findOneBy(["slug" => $slug]);
 
+        if ($post === null) {
+            throw $this->createNotFoundException("Post not found.");
+        }
+
+        $comment = new Comment();
+        $comment->setPost($post);
+
+        $form = $this->createForm(new CommentType(), $comment);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($comment);
+            $em->flush();
+
+            $this->get('session')->getFlashBag()->add('success', 'Comment was submitted');
+
+            return $this->redirectToRoute('blog_core_post_show', ["slug" => $slug]);
+        }
+
+        return [];
     }
 
 }
